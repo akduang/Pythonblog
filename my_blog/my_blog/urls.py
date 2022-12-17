5009c5ea65eea6b89f64e0e835c99e8422e08799
akduang <2019364641@qq.com>
2022-12-17 12:07:50 +0800

    配置app

--- my_blog/my_blog/urls.py
+++ my_blog/my_blog/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+#add include
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
+    #add app url
+    path('article/', include('article.urls', namespace='article'))
 ]
